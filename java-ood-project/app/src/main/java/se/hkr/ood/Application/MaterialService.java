91d4c09f6a717aaa4a3071befc4ebc57c2b41a16
Osikoya Omotoyosi <osikoyanelson@gmail.com>
2026-04-22 14:27:27 +0200

    Implement ProductService and MaterialService skeleton aligned with UML

--- java-ood-project/app/src/main/java/se/hkr/ood/Application/MaterialService.java
+++ java-ood-project/app/src/main/java/se/hkr/ood/Application/MaterialService.java
@@ -1,5 +1,18 @@
 package se.hkr.ood.application;
 
+import se.hkr.ood.domain.Material;
+import se.hkr.ood.domain.MaterialRepository;
+
+import java.util.List;
+
 public class MaterialService {
 
+    public static void createMaterial(String name, int impactValue, List<String> guidance) {
+        Material material = new Material(name, impactValue, guidance);
+        MaterialRepository.create(material);
+    }
+
+    public static Material fetchMaterial(String name) {
+        return MaterialRepository.read(name);
+    }
 }
