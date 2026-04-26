9cfb208ccd1b343b5d26d65733741206630fe154
AdriCJordan <adriancjfn@gmail.com>
2026-04-26 14:58:15 +0200

    Created the static strings for each step within the menu

--- java-ood-project/app/src/main/java/se/hkr/ood/presentation/Menu.java
+++ java-ood-project/app/src/main/java/se/hkr/ood/presentation/Menu.java
@@ -3,7 +3,30 @@ package se.hkr.ood.presentation;
 import se.hkr.ood.application.MaterialService;
 import se.hkr.ood.application.ProductService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Menu {
+  private static List<String> basic = Arrays.asList("a) Fetch Product", "b) List all Products", "c) Other", "q) Exit");
+  private static List<String> a = Arrays.asList("Insert name", "q) Back");
+  private static List<String> a1 = Arrays.asList("The Product doesn't exist", "Create new Product? (Y/N)", "q) Back");
+  private static List<String> a11 = Arrays.asList("Introduce category", "q) Back");
+  private static List<String> a12 = Arrays.asList("Introduce lifespan", "q) Back");
+  private static List<String> a13 = Arrays.asList("Introduce materials (ENTER to confirm)", "q) Back");
+  private static List<String> a131 = Arrays.asList("doesn't exist", "Create new Material? (Y/N)", "q) Back");
+  private static List<String> a132 = Arrays.asList("Introduce impact value", "q) Back");
+  private static List<String> a133 = Arrays.asList("Introduce recycling guidance", "q) Back");
+  private static List<String> a2 = Arrays.asList("Calculate impact value?", "a) Simple Sum Strategy",
+      "b) Weighted by Lifespan Strategy", "q) Back");
+  private static List<String> back = Arrays.asList("q) Back");
+  private static List<String> c = Arrays.asList("a) Update Product", "b) Update Material", "q) Back");
+  private static List<String> ca = Arrays.asList("Insert Product name", "q) Back");
+  private static List<String> ca1 = Arrays.asList("a) Update name", "b) Update category",
+      "c) Update estimated lifespan", "Update Materials", "q) Back");
+  private static List<String> cb = Arrays.asList("Insert Material name", "q) Back");
+  private static List<String> cb1 = Arrays.asList("a) Update name", "b) Update impact value",
+      "c) Update recycling guidance", "q) Back");
 
   public static void startLoop() {
     displayOptions();
