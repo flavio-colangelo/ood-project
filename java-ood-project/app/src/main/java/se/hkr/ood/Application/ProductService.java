d1ed0dcb86236b742302647db86ed7f0df5a0fdd
AdriCJordan <adriancjfn@gmail.com>
2026-04-26 14:56:29 +0200

    Fixed minor type issues

--- java-ood-project/app/src/main/java/se/hkr/ood/Application/ProductService.java
+++ java-ood-project/app/src/main/java/se/hkr/ood/Application/ProductService.java
@@ -2,23 +2,21 @@ package se.hkr.ood.application;
 
 import se.hkr.ood.domain.Product;
 import se.hkr.ood.domain.ProductRepository;
+import se.hkr.ood.domain.RecyclingGuidanceService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService {
 
     public static Product createProduct(String name, String category, int lifespan) {
-        Product product = new Product(name, category, lifespan);
+        Product product = new Product(name, category, lifespan, new ArrayList<>());
         ProductRepository.create(product);
         return product;
     }
 
     public static Product fetchProduct(String name) {
         return ProductRepository.read(name);
-    }
-
-    public static List<Product> listProducts() {
-        return ProductRepository.fetchAll();
     }
 
     public static double environmentalImpact(Product product, EnvironmentalImpactCalculator strategy) {
