0f1e9fee62c6de068900574ad099b689f33fdf69
AdriCJordan <adriancjfn@gmail.com>
2026-04-30 10:44:10 +0200

    Added a toString() method to MenuOption

--- java-ood-project/app/src/main/java/se/hkr/ood/presentation/MenuOption.java
+++ java-ood-project/app/src/main/java/se/hkr/ood/presentation/MenuOption.java
@@ -3,34 +3,39 @@ package se.hkr.ood.presentation;
 import java.util.NoSuchElementException;
 
 public class MenuOption {
-    private String character;
-    private String title;
-    private Runnable function;
+  private String character;
+  private String title;
+  private Runnable function;
 
-    public MenuOption(String character, String title) {
-      this.character = character;
-      this.title = title;
-    }
+  public MenuOption(String character, String title) {
+    this.character = character;
+    this.title = title;
+  }
 
-    public MenuOption(String character, String title, Runnable function) {
-      this.character = character;
-      this.title = title;
-      this.function = function;
-    }
+  public MenuOption(String character, String title, Runnable function) {
+    this.character = character;
+    this.title = title;
+    this.function = function;
+  }
 
-    public void setFunction(Runnable function) { 
-      this.function = function;
-    }
+  public void setFunction(Runnable function) {
+    this.function = function;
+  }
 
-    public void run() {
-      if (function != null) {
-        function.run();
-      } else {
-        throw new NoSuchElementException("The function for this option is missing.");
-      }
-    }
-
-    public String getCharacter() {
-      return this.character;
+  public void run() {
+    if (function != null) {
+      function.run();
+    } else {
+      throw new NoSuchElementException("The function for this option is missing.");
     }
   }
+
+  public String getCharacter() {
+    return this.character;
+  }
+
+  @Override
+  public String toString() {
+    return this.character + ") " + this.title;
+  }
+}
