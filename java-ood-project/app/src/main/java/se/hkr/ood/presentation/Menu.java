b403d19eed88d78c7c2786d1a6932c05e6549148
AdriCJordan <adriancjfn@gmail.com>
2026-04-30 10:44:32 +0200

    Added the initial menu display rom options

--- java-ood-project/app/src/main/java/se/hkr/ood/presentation/Menu.java
+++ java-ood-project/app/src/main/java/se/hkr/ood/presentation/Menu.java
@@ -11,25 +11,39 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-  // private static List<String> basic = Arrays.asList("a) Fetch Product", "b) List all Products", "c) Other", "q) Exit");
+  // private static List<String> basic = Arrays.asList("a) Fetch Product", "b)
+  // List all Products", "c) Other", "q) Exit");
   // private static List<String> a = Arrays.asList("Insert name", "q) Back");
-  // private static List<String> a1 = Arrays.asList("The Product doesn't exist", "Create new Product? (Y/N)", "q) Back");
-  // private static List<String> a11 = Arrays.asList("Introduce category", "q) Back");
-  // private static List<String> a12 = Arrays.asList("Introduce lifespan", "q) Back");
-  // private static List<String> a13 = Arrays.asList("Introduce materials (ENTER to confirm)", "q) Back");
-  // private static List<String> a131 = Arrays.asList("doesn't exist", "Create new Material? (Y/N)", "q) Back");
-  // private static List<String> a132 = Arrays.asList("Introduce impact value", "q) Back");
-  // private static List<String> a133 = Arrays.asList("Introduce recycling guidance", "q) Back");
-  // private static List<String> a2 = Arrays.asList("Calculate impact value?", "a) Simple Sum Strategy",
-  //     "b) Weighted by Lifespan Strategy", "q) Back");
+  // private static List<String> a1 = Arrays.asList("The Product doesn't exist",
+  // "Create new Product? (Y/N)", "q) Back");
+  // private static List<String> a11 = Arrays.asList("Introduce category", "q)
+  // Back");
+  // private static List<String> a12 = Arrays.asList("Introduce lifespan", "q)
+  // Back");
+  // private static List<String> a13 = Arrays.asList("Introduce materials (ENTER
+  // to confirm)", "q) Back");
+  // private static List<String> a131 = Arrays.asList("doesn't exist", "Create new
+  // Material? (Y/N)", "q) Back");
+  // private static List<String> a132 = Arrays.asList("Introduce impact value",
+  // "q) Back");
+  // private static List<String> a133 = Arrays.asList("Introduce recycling
+  // guidance", "q) Back");
+  // private static List<String> a2 = Arrays.asList("Calculate impact value?", "a)
+  // Simple Sum Strategy",
+  // "b) Weighted by Lifespan Strategy", "q) Back");
   // private static List<String> back = Arrays.asList("q) Back");
-  // private static List<String> c = Arrays.asList("a) Update Product", "b) Update Material", "q) Back");
-  // private static List<String> ca = Arrays.asList("Insert Product name", "q) Back");
-  // private static List<String> ca1 = Arrays.asList("a) Update name", "b) Update category",
-  //     "c) Update estimated lifespan", "Update Materials", "q) Back");
-  // private static List<String> cb = Arrays.asList("Insert Material name", "q) Back");
-  // private static List<String> cb1 = Arrays.asList("a) Update name", "b) Update impact value",
-  //     "c) Update recycling guidance", "q) Back");
+  // private static List<String> c = Arrays.asList("a) Update Product", "b) Update
+  // Material", "q) Back");
+  // private static List<String> ca = Arrays.asList("Insert Product name", "q)
+  // Back");
+  // private static List<String> ca1 = Arrays.asList("a) Update name", "b) Update
+  // category",
+  // "c) Update estimated lifespan", "Update Materials", "q) Back");
+  // private static List<String> cb = Arrays.asList("Insert Material name", "q)
+  // Back");
+  // private static List<String> cb1 = Arrays.asList("a) Update name", "b) Update
+  // impact value",
+  // "c) Update recycling guidance", "q) Back");
 
   private static String back = "q) Back";
 
@@ -53,7 +67,7 @@ public class Menu {
         choice = scanner.nextLine();
 
         if (handleExpectedUserInput(choice, "y")) {
-          int step = 1; 
+          int step = 1;
           boolean creatingProduct = true;
 
           String productCategory = "";
@@ -65,8 +79,12 @@ public class Menu {
               case 1:
                 System.out.println("Introduce Category: ");
                 choice = scanner.nextLine();
-                if (isBack(choice)) {step--;}
-                else {productCategory = choice; step++;}
+                if (isBack(choice)) {
+                  step--;
+                } else {
+                  productCategory = choice;
+                  step++;
+                }
                 break;
               case 2:
                 System.out.println("Introduce Lifespan: ");
@@ -81,14 +99,19 @@ public class Menu {
                       } else {
                         isCorrectLifespan = true;
                       }
-                    } else {step--;}
+                    } else {
+                      step--;
+                    }
                   } catch (Exception e) {
                     isCorrectLifespan = false;
                     System.err.println("Lifespan should be a number greater than 0.");
                   }
                 }
-                if (isBack(choice)) {step--;}
-                else {step++;}
+                if (isBack(choice)) {
+                  step--;
+                } else {
+                  step++;
+                }
                 break;
               case 3:
                 System.out.println("Introduce Materials: ");
@@ -97,9 +120,11 @@ public class Menu {
                   System.out.print("> ");
                   String materialInput = scanner.nextLine().trim();
                   if (materialInput.equalsIgnoreCase("q")) {
-                    step--; collectingMaterials = false;
+                    step--;
+                    collectingMaterials = false;
                   } else if (materialInput.isEmpty()) {
-                    step++; collectingMaterials = false;
+                    step++;
+                    collectingMaterials = false;
                   } else {
                     productMaterials.add(materialInput);
                   }
@@ -132,60 +157,53 @@ public class Menu {
   private static void listProductOption() {
     System.out.println("This lists the product!");
   }
-  
+
   private static void runOption(String choice) {
-    MenuOption chosenOption = options.stream().filter(o -> o.getCharacter().equalsIgnoreCase(choice)).findFirst().get(); // get the option with the corresponsing character (or throw NoSuchElement!!)
+    MenuOption chosenOption = options.stream().filter(o -> o.getCharacter().equalsIgnoreCase(choice)).findFirst().get(); // get
+                                                                                                                         // the
+                                                                                                                         // option
+                                                                                                                         // with
+                                                                                                                         // the
+                                                                                                                         // corresponding
+                                                                                                                         // character
+                                                                                                                         // (or
+                                                                                                                         // throw
+                                                                                                                         // NoSuchElement!!)
     chosenOption.run();
   }
-  
+
   private static List<MenuOption> options = new ArrayList<>();
-  
+
   private static void init(Scanner scanner) {
-    options.add(new MenuOption("a", "Fetch Product", ()-> {fetchProductOption(scanner);}));
-    options.add(new MenuOption("b", "List Product", ()-> {listProductOption();}));
+    options.add(new MenuOption("a", "Fetch Product", () -> {
+      fetchProductOption(scanner);
+    }));
+    options.add(new MenuOption("b", "List Product", () -> {
+      listProductOption();
+    }));
   }
 
   public static void startLoop() {
     Scanner scanner = new Scanner(System.in);
-    String choice = scanner.nextLine();
-    boolean looping = true;
     init(scanner);
-    while (!choice.trim().equalsIgnoreCase("q")) {
-      // displayOptions(basic);
-
-      // FOR TESTING
+    String choice;
+    while (true) {
+      displayOptions(options);
+      choice = scanner.nextLine();
+      if (choice.trim().equalsIgnoreCase("q")) {
+        return;
+      }
       try {
         runOption(choice.toLowerCase().trim());
       } catch (Exception e) {
         System.err.println(e.getMessage());
-      } finally {
-        choice = scanner.nextLine();
       }
-
-      // switch (handleUserInput().toLowerCase()) {
-      //   case "a":
-
-      //     break;
-      //   case "b":
-
-      //     break;
-      //   case "c":
-
-      //     break;
-      //   case "q":
-      //     looping = false;
-      //     break;
-      //   default:
-      //     System.out.println("Invalid option, please try again.");
-      //     break;
-      // }
-
     }
   }
 
-  protected static void displayOptions(List<String> options) {
+  protected static void displayOptions(List<MenuOption> options) {
     for (int i = 0; i < options.size(); i++) {
-      System.out.println(options.get(i));
+      System.out.println(options.get(i).toString());
     }
     System.out.print("> ");
   }
