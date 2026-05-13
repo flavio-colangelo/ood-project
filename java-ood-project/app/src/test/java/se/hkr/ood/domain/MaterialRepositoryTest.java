8d3736e80dbba27bf6ef6414b4b9fdbc603fcfa1
Flavio Colangelo <colangeloflavio.personal@gmail.com>
2026-05-13 23:50:13 +0200

    Wrote basic tests for MaterialRepository class

---
+++ java-ood-project/app/src/test/java/se/hkr/ood/domain/MaterialRepositoryTest.java
@@ -0,0 +1,56 @@
+package se.hkr.ood.domain;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaterialRepositoryTest {
+
+    @Test
+    void testCreate() {
+        Material newMaterial = new Material("Cardboard", 5, new ArrayList<>());
+        
+        assertDoesNotThrow(() -> MaterialRepository.create(newMaterial));
+    }
+
+    @Test
+    void testRead() {
+        Material result = MaterialRepository.read("AnyString");
+
+        assertNotNull(result);
+        assertNotNull(result.getGuidance());
+        assertTrue(result.getGuidance().isEmpty());
+    }
+
+    @Test
+    void testDelete() {
+        assertDoesNotThrow(() -> MaterialRepository.delete());
+    }
+
+    @Test
+    void testUpdate() {
+        Material materialToUpdate = new Material();
+        
+        assertDoesNotThrow(() -> MaterialRepository.update("name", "John", materialToUpdate));
+    }
+
+    @Test
+    void testFetchAll() {
+        List<Material> results = MaterialRepository.fetchAll();
+
+        assertNotNull(results);
+        assertTrue(results.isEmpty());
+    }
+
+    @Test
+    void testParse() {
+        Object dummyObject = new Object();
+        Material result = MaterialRepository.parse(dummyObject);
+
+        assertNotNull(result);
+        assertNotNull(result.getGuidance());
+        assertTrue(result.getGuidance().isEmpty());
+    }
+}
