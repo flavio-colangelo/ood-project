17e7682bc3f8d49739312923e9dc4b1d3ac0bbb4
AdriCJordan <adriancjfn@gmail.com>
2026-05-15 14:28:24 +0200

    Created the Recycling Guidance Service Tests

---
+++ java-ood-project/app/src/test/java/se/hkr/ood/domain/RecyclingGuidanceServiceTest.java
@@ -0,0 +1,53 @@
+package se.hkr.ood.domain;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RecyclingGuidanceServiceTest {
+  @Test
+  void fetchGuidanceWithSingleMaterialProduct() {
+    List<String> guidance = new ArrayList<>();
+    guidance.add("This is the recycling guidance");
+    Material material = new Material("materialName", 1, guidance);
+    List<Material> materials = new ArrayList<>();
+    materials.add(material);
+    Product product = new Product("productName", "productCategory", 1, materials);
+
+    assertNull(RecyclingGuidanceService.fetchGuidance(product), "Guidance should not be Null");
+    assertEquals(guidance, RecyclingGuidanceService.fetchGuidance(product),
+        "Guidance should be the same as the one in the sent product material");
+  }
+
+  @Test
+  void fetchGuidanceWithEmptyProduct() {
+    Product product = new Product();
+
+    assertThrows(NullPointerException.class, () -> {
+      RecyclingGuidanceService.fetchGuidance(product);
+    });
+  }
+
+  @Test
+  void fetchGuidanceWithMultipleMaterials() {
+    List<String> guidance1 = new ArrayList<>();
+    guidance1.add("This is the recycling guidance for material 1");
+    Material material1 = new Material("materialName", 1, guidance1);
+    List<String> guidance2 = new ArrayList<>();
+    guidance1.add("This is the recycling guidance for material 2");
+    Material material2 = new Material("materialName", 1, guidance1);
+    List<Material> materials = new ArrayList<>();
+    materials.add(material1);
+    materials.add(material2);
+    Product product = new Product("productName", "productCategory", 1, materials);
+    List<List<String>> guidanceS = new ArrayList<>();
+    guidanceS.add(guidance1);
+    guidanceS.add(guidance2);
+    assertNull(RecyclingGuidanceService.fetchGuidance(product), "Guidance should not be Null");
+    assertEquals(guidanceS, RecyclingGuidanceService.fetchGuidance(product),
+        "Guidance should be the same as the one in the sent product materials");
+  }
+}
