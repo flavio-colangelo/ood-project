23d1492b9846b36703115c583dc3a2cc6563b4ba
Flavio Colangelo <colangeloflavio.personal@gmail.com>
2026-05-15 14:22:16 +0200

    Added EnvironmentalImpactCalculator concrete class testing

---
+++ java-ood-project/app/src/test/java/se/hkr/ood/WeightedByLifespanTest.java
@@ -0,0 +1,41 @@
+package se.hkr.ood;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import se.hkr.ood.domain.EnviromentalImpactCalculator;
+import se.hkr.ood.domain.Material;
+import se.hkr.ood.domain.Product;
+import se.hkr.ood.domain.SimpleSumStrategy;
+import se.hkr.ood.domain.WeightedByLifespanStrategy;
+
+public class WeightedByLifespanTest {
+    @Test
+    void testCalculate() {
+        EnviromentalImpactCalculator strategy = new WeightedByLifespanStrategy();
+        List<String> expectedGuidance1 = Arrays.asList("Rinse thoroughly", "Give to Adrian");
+        Material material1 = new Material("PET Plastic", 50, expectedGuidance1);
+        List<String> expectedGuidance2 = Arrays.asList("Eat thoroughly", "Give to Nelson");
+        Material material2 = new Material("Teflon", 90, expectedGuidance2);
+
+        List<Material> materials = Arrays.asList(material1, material2);
+
+        Product newProduct = new Product("John", "Human", 27, materials);
+
+        assertNotNull(strategy.calculate(newProduct), "The Simple Sum Strategy should not be null");
+
+        double expectedImpact = 0;
+
+        for (Material material : materials) {
+            expectedImpact += material.getImpact();
+        }
+        
+        assertEquals(strategy.calculate(newProduct), expectedImpact / materials.size(), "The Simple Sum Strategy should return the correct value");
+    }
+}
